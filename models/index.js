c4f337b32f410393988eac1868f5ef9d1b3455c1
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-07-21 17:11:15 -0700

    added npm express

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/project-1-wheres-my-bike");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project-1-wheres-my-bike");
 
 module.exports.Location = require("./location.js");
 // module.exports.User = require("./user.js");
