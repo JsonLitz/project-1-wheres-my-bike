1cf2769c631fe498a6258f89eff31e29c06f66f9
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-07-23 13:07:23 -0700

    started work on authentication

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 // SERVER-SIDE JAVASCRIPT
+// require express and other modules
+var express = require('express'),
+    app = express(),  //generate a new express app and call it 'app'
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 
-//require express in our app
-var express = require('express');
 
-var bodyParser = require('body-parser');
-// generate a new express app and call it 'app'
-var app = express();
+// configure bodyParser (for receiving form data)
+app.use(bodyParser.urlencoded({ extended: true }));
+// serve static files from public folder
 app.use(express.static(__dirname + '/public'));
-
-app.use(bodyParser.urlencoded({
-  extended: true
+// set view engine to hbs (handlebars)
+app.set('view engine', 'hbs');
+// middleware for auth
+app.use(cookieParser());
+app.use(session({
+  secret: 'secretkey', // change this!
+  resave: false,
+  saveUninitialized: false
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+// require Post model
+var db = require('./models'),
+    Post = db.Post;
+    User = db.User;
+
+// passport config
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
 /*
  * JSON API Endpoints
  */
